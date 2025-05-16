07a55834176e2d210f356d215db99af8a95db97a
Steven.Zheng <steven.zheng@tm-robot.com>
2025-05-16 19:11:16 +0800

    feat: add image sub node to show image

---
+++ src/eih_client/src/ImageSubNode.cpp
@@ -0,0 +1,38 @@
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+class ImageSubscriberNode : public rclcpp::Node {
+ public:
+  ImageSubscriberNode() : Node("image_subscriber_node") {
+    image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "eih_image", 10,
+        std::bind(&ImageSubscriberNode::image_callback, this,
+                  std::placeholders::_1));
+  }
+
+ private:
+  void image_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
+    try {
+      cv::Mat image = cv_bridge::toCvCopy(msg, "bgr8")->image;
+      cv::Mat resized_image;
+      cv::resize(image, resized_image, cv::Size(640, 480));
+
+      cv::imshow("Subscribed Image", resized_image);
+      cv::waitKey(1);
+    } catch (const cv_bridge::Exception &e) {
+      RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+    }
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
+};
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ImageSubscriberNode>());
+  rclcpp::shutdown();
+  return 0;
+}
