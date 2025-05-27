a8666ac24bdc2d7e7badb63728003f05776c6138
Steven.Zheng <steven.zheng@tm-robot.com>
2025-05-27 10:05:49 +0800

    refactor(root): move files to new directory structure for ros package

--- launch/eih_client_pub.launch.py
+++ launch/eih_client_pub.launch.py
@@ -6,7 +6,7 @@ from launch.actions import DeclareLaunchArgument
 def generate_launch_description():
     robot_ip_arg = DeclareLaunchArgument(
         'robot_ip',
-        default_value='172.25.181.19',  # 預設值, 之後拿掉
+        default_value='172.25.181.19',  # for test, will remove
         description='Server address for the robot EIH'
     )
 
